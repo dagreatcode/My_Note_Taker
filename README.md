f8fabe788d9518c8ce24796d4bd55cbb71853f48
dagreatcode <dagreatcode@gmail.com>
2020-10-01 17:14:36 -0400

    updating my server file with come classwork content

--- README.md
+++ README.md
@@ -6,5 +6,14 @@
 
 ## Time To Start
 
-Post Route
 Get Route
+Post Route
+Delete
+
+## User Story
+
+AS A user, I want to be able to write and save notes
+
+I WANT to be able to delete notes I've written before
+
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
--- server.js
+++ server.js
@@ -13,7 +13,14 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Post Route
+
 
 
 //Get Route
+//* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
+//Post Route
+//* POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+//Delete
+//  * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
