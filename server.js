feacafcb90701d55141a741309860695b8639cf9
dagreatcode <dagreatcode@gmail.com>
2020-10-25 22:03:19 -0400

    Crud almost set up the post and delete

--- server.js
+++ server.js
@@ -36,23 +36,15 @@ app.get("/api/notes", function(req, res) {
 //* POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 app.post("/api/notes", function (req, res){
-  console.log(req.body);
-  fs.readFile("./db.json", "utf-8", (err, data) => {
-      if(err) {
-        console.log(err);
-        return res.status(500).json({
-            error: true,
-            data: null,
-            message: "Error"
-        })
-      }
-  })
+
 });
 
 //Delete
 //  * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
-
+app.delete("/api/notes", function (req, res){
+  
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
