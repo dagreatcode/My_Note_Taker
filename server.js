7c1ca1b82c31835b7d58a31a29985966640370a5
dagreatcode <dagreatcode@gmail.com>
2020-10-25 22:58:00 -0400

    finally can see the site

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const db =  require("./db/db.json");
+// const db =  require("./db/db.json");
 
 // Sets up the Express App
 // =============================================================
@@ -30,7 +30,7 @@ app.get("/notes", function(req, res) {
 //* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
 app.get("/api/notes", function (req, res){
-  console.log("post routes")
+  console.log("get routes");
   fs.readFile("./db/db.json", function(err, data){
     if (err) throw err;
     console.log(json.parse(data));
@@ -43,14 +43,20 @@ app.get("/api/notes", function (req, res){
 //* POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 app.post("/api/notes", function (req, res){
-  
+  console.log("post routes");
+  fs.readFile("db/db.json", function (err,data) {
+    
+  });
 });
 
 //Delete
 //  * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 app.delete("/api/notes", function (req, res){
-  
+  console.log("delete routes");
+  fs.readFile("db/db.json", function (err,data) {
+
+  });
 });
 
 app.listen(PORT, () => {
